ded0535b3ae66d78f2cfbbe31f01dd9ae505334f
aditpgnSkom <littlecommander46@gmail.com>
2024-04-21 16:38:54 +0700

    feat - sidebar admin

--- src/components/admin/SidebarUI/index.jsx
+++ src/components/admin/SidebarUI/index.jsx
@@ -10,11 +10,34 @@ function SidebarUI() {
           <Sidebar.Item as={Link} to="/" icon={BiSolidDashboard}>
             Dashboard
           </Sidebar.Item>
-          <Sidebar.Collapse icon={BiSolidDashboard} label="Dashboard">
-            <Sidebar.Item as={Link} to="/" icon={BiSolidDashboard}>
-              Dashboard
+          <Sidebar.Collapse icon={BiSolidDashboard} label="Guru">
+            <Sidebar.Item as={Link} to="/mapel" icon={BiSolidDashboard}>
+              Mata Pelajaran
+            </Sidebar.Item>
+            <Sidebar.Item as={Link} to="/materi" icon={BiSolidDashboard}>
+              Materi
             </Sidebar.Item>
           </Sidebar.Collapse>
+          <Sidebar.Collapse icon={BiSolidDashboard} label="Murid">
+            <Sidebar.Item as={Link} to="/absensi" icon={BiSolidDashboard}>
+              Absensi
+            </Sidebar.Item>
+            <Sidebar.Item as={Link} to="/nilai" icon={BiSolidDashboard}>
+              Nilai
+            </Sidebar.Item>
+          </Sidebar.Collapse>
+          <Sidebar.Item as={Link} to="/jadwal" icon={BiSolidDashboard}>
+            Jadwal
+          </Sidebar.Item>
+          <Sidebar.Item as={Link} to="/kelas" icon={BiSolidDashboard}>
+            Kelas
+          </Sidebar.Item>
+          <Sidebar.Item as={Link} to="/pengumuman" icon={BiSolidDashboard}>
+            Pengumuman
+          </Sidebar.Item>
+          <Sidebar.Item as={Link} to="/user" icon={BiSolidDashboard}>
+            User
+          </Sidebar.Item>
         </Sidebar.ItemGroup>
       </Sidebar.Items>
     </Sidebar>
