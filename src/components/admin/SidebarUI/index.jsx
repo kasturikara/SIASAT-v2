6412015ddd4267f844f2adc9419168cca7e57054
aditpgnSkom <littlecommander46@gmail.com>
2024-04-22 20:36:47 +0700

    fix - icon sidebar admin

--- src/components/admin/SidebarUI/index.jsx
+++ src/components/admin/SidebarUI/index.jsx
@@ -1,41 +1,53 @@
 import { Sidebar } from "flowbite-react";
 import { Link } from "react-router-dom";
-import { BiSolidDashboard } from "react-icons/bi";
+import {
+  AiOutlineApartment,
+  AiOutlineBell,
+  AiOutlineClockCircle,
+  AiOutlineDashboard,
+  AiOutlinePercentage,
+  AiOutlineProfile,
+  AiOutlineRead,
+  AiOutlineSchedule,
+  AiOutlineSolution,
+  AiOutlineTeam,
+  AiOutlineUser,
+} from "react-icons/ai";
 
 function SidebarUI() {
   return (
     <Sidebar className="fixed top-0 left-0 z-40 w-56 h-screen -translate-x-full border-r border-gray-200 md:translate-x-0 pt-14">
       <Sidebar.Items>
         <Sidebar.ItemGroup>
-          <Sidebar.Item as={Link} to="/" icon={BiSolidDashboard}>
+          <Sidebar.Item as={Link} to="/" icon={AiOutlineDashboard}>
             Dashboard
           </Sidebar.Item>
-          <Sidebar.Collapse icon={BiSolidDashboard} label="Guru">
-            <Sidebar.Item as={Link} to="/mapel" icon={BiSolidDashboard}>
-              Mata Pelajaran
+          <Sidebar.Collapse icon={AiOutlineTeam} label="Guru">
+            <Sidebar.Item as={Link} to="/mapel" icon={AiOutlineRead}>
+              Mapel
             </Sidebar.Item>
-            <Sidebar.Item as={Link} to="/materi" icon={BiSolidDashboard}>
+            <Sidebar.Item as={Link} to="/materi" icon={AiOutlineSolution}>
               Materi
             </Sidebar.Item>
           </Sidebar.Collapse>
-          <Sidebar.Collapse icon={BiSolidDashboard} label="Murid">
-            <Sidebar.Item as={Link} to="/absensi" icon={BiSolidDashboard}>
+          <Sidebar.Collapse icon={AiOutlineUser} label="Murid">
+            <Sidebar.Item as={Link} to="/absensi" icon={AiOutlineClockCircle}>
               Absensi
             </Sidebar.Item>
-            <Sidebar.Item as={Link} to="/nilai" icon={BiSolidDashboard}>
+            <Sidebar.Item as={Link} to="/nilai" icon={AiOutlinePercentage}>
               Nilai
             </Sidebar.Item>
           </Sidebar.Collapse>
-          <Sidebar.Item as={Link} to="/jadwal" icon={BiSolidDashboard}>
+          <Sidebar.Item as={Link} to="/jadwal" icon={AiOutlineSchedule}>
             Jadwal
           </Sidebar.Item>
-          <Sidebar.Item as={Link} to="/kelas" icon={BiSolidDashboard}>
+          <Sidebar.Item as={Link} to="/kelas" icon={AiOutlineApartment}>
             Kelas
           </Sidebar.Item>
-          <Sidebar.Item as={Link} to="/pengumuman" icon={BiSolidDashboard}>
+          <Sidebar.Item as={Link} to="/pengumuman" icon={AiOutlineBell}>
             Pengumuman
           </Sidebar.Item>
-          <Sidebar.Item as={Link} to="/user" icon={BiSolidDashboard}>
+          <Sidebar.Item as={Link} to="/user" icon={AiOutlineProfile}>
             User
           </Sidebar.Item>
         </Sidebar.ItemGroup>
