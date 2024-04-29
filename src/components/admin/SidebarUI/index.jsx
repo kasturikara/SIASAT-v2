a4a57a2043473eb8c42cce81ec34cc6a15a44127
aditpgnSkom <littlecommander46@gmail.com>
2024-04-29 11:24:45 +0700

    fix - color

--- src/components/admin/SidebarUI/index.jsx
+++ src/components/admin/SidebarUI/index.jsx
@@ -1,6 +1,7 @@
 import { Sidebar } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import {
+  AiFillBell,
   AiOutlineApartment,
   AiOutlineBell,
   AiOutlineClockCircle,
@@ -15,39 +16,92 @@ import {
 } from "react-icons/ai";
 
 function SidebarUI() {
+  const location = useLocation();
+
   return (
     <Sidebar className="fixed top-0 left-0 z-40 w-56 h-screen -translate-x-full border-r border-gray-200 md:translate-x-0 pt-14">
       <Sidebar.Items>
         <Sidebar.ItemGroup>
-          <Sidebar.Item as={Link} to="/" icon={AiOutlineDashboard}>
+          <Sidebar.Item
+            as={Link}
+            to="/"
+            icon={AiOutlineDashboard}
+            active={location.pathname === "/"}
+          >
             Dashboard
           </Sidebar.Item>
           <Sidebar.Collapse icon={AiOutlineTeam} label="Guru">
-            <Sidebar.Item as={Link} to="/mapel" icon={AiOutlineRead}>
+            <Sidebar.Item
+              as={Link}
+              to="/mapel"
+              icon={AiOutlineRead}
+              active={location.pathname === "/mapel"}
+            >
               Mapel
             </Sidebar.Item>
-            <Sidebar.Item as={Link} to="/materi" icon={AiOutlineSolution}>
+            <Sidebar.Item
+              as={Link}
+              to="/materi"
+              icon={AiOutlineSolution}
+              active={location.pathname === "/materi"}
+            >
               Materi
             </Sidebar.Item>
           </Sidebar.Collapse>
           <Sidebar.Collapse icon={AiOutlineUser} label="Murid">
-            <Sidebar.Item as={Link} to="/absensi" icon={AiOutlineClockCircle}>
+            <Sidebar.Item
+              as={Link}
+              to="/absensi"
+              icon={AiOutlineClockCircle}
+              active={location.pathname === "/absensi"}
+            >
               Absensi
             </Sidebar.Item>
-            <Sidebar.Item as={Link} to="/nilai" icon={AiOutlinePercentage}>
+            <Sidebar.Item
+              as={Link}
+              to="/nilai"
+              icon={AiOutlinePercentage}
+              active={location.pathname === "/nilai"}
+            >
               Nilai
             </Sidebar.Item>
           </Sidebar.Collapse>
-          <Sidebar.Item as={Link} to="/jadwal" icon={AiOutlineSchedule}>
+          <Sidebar.Item
+            as={Link}
+            to="/jadwal"
+            icon={AiOutlineSchedule}
+            active={location.pathname === "/jadwal"}
+          >
             Jadwal
           </Sidebar.Item>
-          <Sidebar.Item as={Link} to="/kelas" icon={AiOutlineApartment}>
+          <Sidebar.Item
+            as={Link}
+            to="/kelas"
+            icon={AiOutlineApartment}
+            active={location.pathname === "/kelas"}
+          >
             Kelas
           </Sidebar.Item>
-          <Sidebar.Item as={Link} to="/pengumuman" icon={AiOutlineBell}>
+          <Sidebar.Item
+            as={Link}
+            to="/pengumuman"
+            icon={
+              location.pathname !== "/pengumuman" ? AiOutlineBell : AiFillBell
+            }
+            active={location.pathname === "/pengumuman"}
+            className={` hover:bg-teal-400 hover:text-gray-400 ${
+              location.pathname === "/pengumuman" &&
+              "bg-teal-300 text-gray-700 font-medium"
+            } `}
+          >
             Pengumuman
           </Sidebar.Item>
-          <Sidebar.Item as={Link} to="/user" icon={AiOutlineProfile}>
+          <Sidebar.Item
+            as={Link}
+            to="/user"
+            icon={AiOutlineProfile}
+            active={location.pathname === "/user"}
+          >
             User
           </Sidebar.Item>
         </Sidebar.ItemGroup>
